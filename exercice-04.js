92bb3273ea61651fe48e13ec57757468499d3f5f
chahed mazouz <mazouzchahd12@gmail.com>
2026-09-01 20:59:35 +0100

    Exercice 04 — La liste de courses

--- exercice-04.js
+++ exercice-04.js
@@ -6,6 +6,19 @@ const course = courses.indexOf("lait")
 
 courses.splice(course, 1)
 
-console.log(courses)
-console.log(course)
+console.log(`nombre d'articles: ${courses.length}`)
+
+for(let i = 0; i < courses.length; i++){
+    console.log(`${i + 1} . ${courses[i]}`)
+}
+
+courses.includes('cafe')
+if(courses.includes('cafe')){
+    console.log("le cafe est bien dans la list")
+}
+else{
+    console.log("le cafe n'est pas dans la list")
+}
+
+
 
