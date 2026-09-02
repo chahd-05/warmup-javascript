8674b1779bb7913181175679e18c147e75b79bab
chahed mazouz <mazouzchahd12@gmail.com>
2026-09-02 18:06:54 +0100

    Exercice 09 — La remise fidélité

--- exercice-09.js
+++ exercice-09.js
@@ -4,6 +4,22 @@ const commandes = [
 { montant: 1200, statut: "premium" }
 ];
 
-function calculerCommande(montant, statut){
-    
+function calculerCommande(montant, statut) {
+    let pourcentageRemise;
+
+    if (montant < 200) {
+        pourcentageRemise = 0;
+    } else if (montant < 500) {
+        pourcentageRemise = 5;
+    } else if (montant < 1000) {
+        pourcentageRemise = 10;
+    } else {
+        pourcentageRemise = 15;
+    }
+
+    return pourcentageRemise;
 }
+
+console.log(calculerCommande(150, "standard"));
+console.log(calculerCommande(620, "standard"));
+console.log(calculerCommande(1200, "premium"));
