f2a14424a7390dfb5d5283c998a2dd350bac9f9d
chahed mazouz <mazouzchahd12@gmail.com>
2026-09-03 17:09:37 +0100

    feat: add function for generate a report

--- exercice-10.js
+++ exercice-10.js
@@ -31,3 +31,43 @@ const moy = avg / 3
 
 console.log(moy)
 
+function genererRapport(ventes) {
+
+    const total = ventes.reduce(function(a, b) {
+        return a + b.montant;
+    }, 0);
+
+    const find = ventes.reduce(function(a, b) {
+        return b.montant > a.montant ? b : a;
+    });
+
+    const ca = caParVendeur(ventes);
+
+    const moyenne = total / 3;
+
+    const vendeurs = Object.keys(ca);
+
+    const auDessus = vendeurs.filter(function(vendeur) {
+        return ca[vendeur] > moyenne;
+    });
+
+    let rapport = "=== RAPPORT DES VENTES ===\n";
+
+    rapport += "Chiffre d'affaires total : " + total + " DH\n";
+
+    rapport += "Meilleure vente : " + find.produit + " (" + find.vendeur + ") - " + find.montant + " DH\n";
+
+    rapport += "CA par vendeur :\n";
+
+    rapport += "Amina : " + ca.Amina + " DH\n";
+    rapport += "Youssef : " + ca.Youssef + " DH\n";
+    rapport += "Sara : " + ca.Sara + " DH\n";
+
+    rapport += "Moyenne par vendeur : " + moyenne.toFixed(2) + " DH\n";
+
+    rapport += "Au-dessus de la moyenne : " + auDessus.join(", ");
+
+    return rapport;
+}
+
+console.log(genererRapport(ventes));
