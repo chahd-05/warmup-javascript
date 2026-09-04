9dd62a34821c48b995d138412210e442e70abeb5
chahed mazouz <mazouzchahd12@gmail.com>
2026-09-04 11:27:11 +0100

    feat: add memory card data

---
+++ memory-card-game/script.js
@@ -0,0 +1,33 @@
+const cards = [
+    {
+        id: 1,
+        name: "cat",
+        image: "assets/images/cat.png"
+    },
+    {
+        id: 2,
+        name: "dog",
+        image: "assets/images/dog.png"
+    },
+    {
+        id: 3,
+        name: "lion",
+        image: "assets/images/lion.png"
+    },
+    {
+        id: 4,
+        name: "elephant",
+        image: "assets/images/elephant.png"
+    },
+    {
+        id: 5,
+        name: "tiger",
+        image: "assets/images/tiger.png"
+    },
+    {
+        id: 6,
+        name: "panda",
+        image: "assets/images/panda.png"
+    }
+];
+console.log(cards)
